aabe42d025c910ab153f9a458ccf5ccf100ed259
max-r-33 <max.rodewald@gmail.com>
2019-01-07 12:02:07 -0800

    comments and making search more specific to get better results

--- scraper.py
+++ scraper.py
@@ -6,20 +6,24 @@ from user_agent import generate_user_agent
 # generating user agent so we don't get blocked from requesting the site
 headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
 
+# returns array of urls to request
+# takes in amount of pages to scrape
 def getUrls(pagesToScrape):
   urls = [];
   currPage = 1;
   while(currPage <= pagesToScrape):
-    baseUrl = 'https://www.zillow.com/homes/for_sale/Santa-Cruz-CA/pmf,pf_pt/13715_rid/globalrelevanceex_sort/37.187672,-121.807824,36.902136,-122.342034_rect/10_zm/' + str(currPage) + '_p/'
+    baseUrl = 'https://www.zillow.com/homes/for_sale/Santa-Cruz-CA/fsba,fsbo_lt/13715_rid/globalrelevanceex_sort/37.187672,-121.807824,36.902136,-122.342034_rect/10_zm/' + str(currPage) + '_p/0_mmm/'
     urls.append(baseUrl)
     currPage = currPage + 1
   return urls;
 
+# removes text from strings
 def sanitize(str):    
   return float(re.sub('[,$K]', '', str))
 
-def scrape():
-    urls = getUrls(10);
+# scrapes specified number of pages
+def scrape(numPages):
+    urls = getUrls(numPages);
     results = [];
 
     for url in urls:
@@ -33,11 +37,13 @@ def scrape():
         homePrices = [];
         homeStats = [];
 
+        # parses out price for each home
         for price in prices :
           homePrice = sanitize(price.get_text())
           homePrices.append(homePrice)
 
         for idx, propertyInfo in enumerate(info):
+          # parses out beds, baths, sqft for each house
           try:
             parsedInfo = propertyInfo.get_text().split(' · ')
             beds = 1.0 if parsedInfo[0] in ('Studio', '1 bd') else sanitize(parsedInfo[0].split(' bds')[0]);
@@ -47,12 +53,14 @@ def scrape():
           except:
             homeStats.append([0,0,0]);
             print("error parsing");
-        result = [[price] + homeStats[i] for i, price in enumerate(homePrices)]
-        results.append(result);
+        
+        # appending array of stats for all homes on a page
+        results.append([[price] + homeStats[i] for i, price in enumerate(homePrices)])
       else:
         print("Error:")
         print(site.status_code)
 
+    # return flatten(results);
     count = 0;
     for page in results:
       for home in page:
@@ -64,4 +72,4 @@ def scrape():
         print("Sqft: ", home[3]);
     print("Scraped results for " + str(count) + " properties")
 
-scrape()
+scrape(10)
